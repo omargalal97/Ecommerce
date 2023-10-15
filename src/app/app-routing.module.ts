08c62715f46e956f3ba7c71b06ae6ca64773ceab
omargalal97 <omargalal95210@gmail.com>
2023-10-15 11:56:08 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeListComponent } from './home-list/home-list.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import {AddNewProductComponent} from './add-new-product/add-new-product.component'
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'Register', component: RegistrationComponent },
+  { path: 'HomeList', component: HomeListComponent },
+  { path: 'HomeDetails', component: HomeDetailsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'productdetails', component: ProductDetailsComponent },
+  { path: 'add-new-product', component: AddNewProductComponent },
+  { path: 'Login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
+import { HomeListComponent } from './home-list/home-list.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AddNewProductComponent } from './add-new-product/add-new-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    HomeListComponent,
+    HomeDetailsComponent,
+    ProductDetailsComponent,
+    AddNewProductComponent
   ],
   imports: [
     BrowserModule,
